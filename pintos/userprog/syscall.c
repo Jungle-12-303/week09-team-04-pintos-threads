e75d0911fa9062263eebf092aeae993523ed4ca3
HyeYeonLee0112 <aa99509188@gmail.com>
2026-05-06 21:45:47 +0900

    docs: sys_read 함수 설명 주석 제거
    sys_read 함수에 대한 주석을 제거하여 코드 가독성을 높임.

--- pintos/userprog/syscall.c
+++ pintos/userprog/syscall.c
@@ -187,20 +187,6 @@ sys_read (uint64_t fd, uint64_t buffer, uint64_t size) {
 	return 0;
 }
 
-/* @note
-Writes size bytes from buffer to the open file fd.
-Returns the number of bytes actually written,
-which may be less than size if some bytes could not be written.
-Writing past end-of-file would normally extend the file,
-but file growth is not implemented by the basic file system.
-The expected behavior is to write as many bytes as possible up to end-of-file and return the actual number written,
-or 0 if no bytes could be written at all. fd 1 writes to the console.
-Your code to write to the console should write all of buffer in one call to putbuf(),
-at least as long as size is not bigger than a few hundred bytes
-(It is reasonable to break up larger buffers).
-Otherwise, lines of text output by different processes may end up interleaved on the console,
-confusing both human readers and our grading scripts.
-*/
 static int
 sys_write (uint64_t fd, uint64_t buffer, uint64_t size) {
 	if (fd == STDOUT_FILENO) {
